2b8962a969e7bdcc7ad659e911854cbc8cfbdd3d
c9czamat <c9czamat@vasvari.org>
2025-02-13 08:42:47 +0100

    HttpService Injektálás

--- Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace Kreta.Desktop.ViewModels.SchoolSubjects
 {
@@ -7,7 +9,17 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
     {
         private readonly ISubjectHttpService _httpService;
 
+        //1.b Konstruktorban injektáljuk a ISubjectHttpService-t
+        public SubjectsManagmentViewModel(ISubjectHttpService? httpService)
+        {
+           _httpService=httpService?? throw new ArgumentNullException(nameof(httpService));
+        }
+
         //1.feladat: tantárgyak lekérése a backendről (vizsgaremek)
         //1.a Adatok menüpont kiválasztására jelenjenek meg: InitializeAsync
+        public override Task InitializeAsync()
+        {
+            return base.InitializeAsync();
+        }
     }
 }
