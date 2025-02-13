6e64f16c548d0c913eb57ad6bff7ab8dee167be7
c9czamat <c9czamat@vasvari.org>
2025-02-13 09:05:00 +0100

    Alapértlemezett konstriktornak mindig lennie kell, ebben az állapotban már tesztelhető a kód

--- Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService.Services;
+using Kreta.Shared.Models.Entites;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace Kreta.Desktop.ViewModels.SchoolSubjects
 {
     public partial class SubjectsManagmentViewModel : BaseViewModel
     {
         private readonly ISubjectHttpService _httpService;
+        public SubjectsManagmentViewModel()
+        {
+            
+        }
 
         //1.b Konstruktorban injektáljuk a ISubjectHttpService-t
         public SubjectsManagmentViewModel(ISubjectHttpService? httpService)
@@ -17,9 +24,17 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
 
         //1.feladat: tantárgyak lekérése a backendről (vizsgaremek)
         //1.a Adatok menüpont kiválasztására jelenjenek meg: InitializeAsync
-        public override Task InitializeAsync()
+        public override async Task InitializeAsync()
         {
-            return base.InitializeAsync();
+            await UpdateViewAsync();
+            base.InitializeAsync();
+        }
+
+        //1.c Adatok lekérése a backendről
+        private async Task UpdateViewAsync()
+        {
+            //1.d HttpService-en kerszetül backend hívás
+            List<Subject>  subjects= await _httpService.GetAllAsync();
         }
     }
 }
